ba974d57905b0c09e435c7a7936fdc64c0982322
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-09 10:29:58 -0600

    se inicializa proyecto

---
+++ src/main/java/org/IJGZ20240909/Ijgz20240909CtrlProyectApiApplication.java
@@ -0,0 +1,13 @@
+package org.IJGZ20240909;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ijgz20240909CtrlProyectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ijgz20240909CtrlProyectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/IJGZ20240909/Ijgz20240909CtrlProyectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.IJGZ20240909;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ijgz20240909CtrlProyectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
