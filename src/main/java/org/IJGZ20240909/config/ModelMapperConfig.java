3343fd072c994f7eed9f077917b65de66c8e1b3e
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-09 21:23:52 -0600

    Reposirotio de product y configuración de modelmapper

---
+++ src/main/java/org/IJGZ20240909/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package org.IJGZ20240909.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
---
+++ src/main/java/org/IJGZ20240909/repositorios/IProductIJGZRepository.java
@@ -0,0 +1,7 @@
+package org.IJGZ20240909.repositorios;
+
+import org.IJGZ20240909.modelos.ProductIJGZ;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface IProductIJGZRepository extends JpaRepository<ProductIJGZ, Integer>{
+
+}
